667b145570044229304e24ac188555a4f26ef213
Puneet Chauhan <50094501+puchauhan2@users.noreply.github.com>
2024-01-25 22:18:12 +0530

    added printing row
    added printing row

---
+++ shell_script/printing row filels.sh
@@ -0,0 +1,24 @@
+ls *.* > files
+
+files=$(ls *.*)
+
+contentNum=$(ls *.* | wc -l files | awk '{print $1}')
+
+#filename=$(sed 's/\./ /g' files | awk '{print $1}')
+
+extention=$(sed 's/\./ /g' files | awk '{print $2}')
+
+echo $contentNum
+
+for r in `seq 1 $contentNum`
+do
+  
+ name=$( echo $files | awk '{printf $"'$r'"}' )
+ type=$( echo $extention | awk '{printf $"'$r'"}' )
+  
+  echo $name
+  echo $type
+
+done
+
+
