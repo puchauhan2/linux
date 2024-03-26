442dfdd463516cd9ddb1d91491e8aad1c8894a51
Puneet Chauhan <50094501+puchauhan2@users.noreply.github.com>
2024-03-26 15:49:50 +0530

    pushed new vfat code
    pushed new vfat code

--- shell_script/vfat_password_lock/check_vfat.bash
+++ shell_script/vfat_password_lock/check_vfat.bash
@@ -1,35 +1,87 @@
-packages=("wow" "lsof" "sysstat" "virt-what" "dmidecode" "bc" "attr" "binutils" "cifs-utils" "dhclient" "dosfstools" "ethtool" "gawk" "lsb-release" "xorriso" "grub2-pc" "nfs-common" "openssl" "parted" "util-linux" "iputils-ping" "kbd" "e2fsprogs" "fdisk" "gzip" "iproute2" "lsb-release" "net-tools" "isolinux" "syslinux" "syslinux-common" "tar" "libxml2-devel" "sysvinit-tools" "brutils-2.5" "lzop")
+#!/bin/bash
+os_flavour_type=`hostnamectl | grep "Operating System" | awk '{print $3}'`;
 
-source /etc/os-release
+BR='\033[1;31m'     # ${BR}
+BG='\033[1;32m'     # ${BG}
+NC='\033[0m'        # ${NC}
+BY='\033[1;33m'     # ${BY}
+m100='\U01F4AF'     # ${m100}
+cross='\u274c\n'    # ${cross}
 
-oracle_pkg() {
-    for pkg in ${packages[@]};
-     do check=`yum -q list installed $pkg 2>&1 /dev/null`; 
-        if [ $? -ne 0 ]; then
-        echo "${pkg} not_installed.";
-        else 
-        echo "${pkg} is_installed.";
-        fi 
-    done
+function add_password(){
+    username='gecloud'
+    password='$6$y5Nkq./rYUPNrwXw$7XHMGjC93HwLRnqeGr4RC.4GYyvUnqML/3yKKsejWn7OfH6Lzfn7sJ1EE9j.y5yNWrEBHNE9gq/pGLYg4qeSx1' # Password  hash sha512
+    echo ${username}:${password} | chpasswd -e
+    errorCode1=${?}
+    if [[ ${errorCode1} = 0 ]]
+    then
+    echo -e "\n${BY}OutPut :${BG} Password added to ${username} user ${m100}${NC}"
+    else
+    echo -e "\n${BY}OutPut :${BR} Error while adding Password to ${username} ${cross}"
+    fi
 }
 
-Ubuntu_exec(){
-for pkg in ${packages[@]}; do
-    pkg_info=`apt list $pkg 2> /dev/null |grep -i installed | sed 's/\// /g'  | awk '{print $1}'`
-    if [ "${pkg}" == "${pkg_info}" ]; then
-		echo "${pkg} is_installed"
-	else
-		echo "${pkg} not_installed"
-	fi
-done
+function enable_vfat(){
 
+    grep '#install fat /bin/true' /etc/modprobe.d/dev-sec.conf
+    errorCode5=${?}
+    grep '#install vfat /bin/true' /etc/modprobe.d/dev-sec.conf
+    errorCode6=${?}
+
+    if [[ ${errorCode5} = 0 ]] && [[ ${errorCode6} = 0 ]]
+    then 
+    modprobe -v vfat && echo -e "${BY}OutPut :${BG} UnModified Config: Enabled Vfat ${m100}${NC}"
+    else 
+    echo -e "Taking Backup of config file"
+    cp -p /etc/modprobe.d/dev-sec.conf /etc/modprobe.d/dev-sec.conf_bk && echo -e "${BG} Backup configuration Completed ${m100}${NC}"
+    sed -i 's:install vfat /bin/true:#install vfat /bin/true:g'  /etc/modprobe.d/dev-sec.conf
+    sed -i 's:install fat /bin/true:#install fat /bin/true:g'  /etc/modprobe.d/dev-sec.conf
+    modprobe -v vfat && echo -e "${BY}OutPut :${BG} Modified Config: Enabled Vfat ${m100}${NC}"
+    fi
 }
-case ${ID} in 
-"debain") echo "Ubuntu linux not supported as of now" ;; 
-"CentOS") oracle_pkg ;; 
-"SUSE") echo "SUSE linux not supported" ;; 
-"Red") oracle_pkg ;; 
-"Oracle") oracle_pkg ;; 
-"Amazon") oracle_pkg ;; 
-*) echo -e "OS Type not detected ";; 
+
+function check_vfat_cfg(){
+    grep 'install fat /bin/true' /etc/modprobe.d/dev-sec.conf
+    errorCode3=${?}
+    grep 'install vfat /bin/true' /etc/modprobe.d/dev-sec.conf
+    errorCode4=${?}
+
+    if [[ ${errorCode3} = 0 ]] && [[ ${errorCode4} = 0 ]]
+    then 
+    echo -e "${BG} Vfat configuration file found,going to enable Vfat ${m100}${NC}"
+    enable_vfat
+    else 
+    echo -e "${BY}OutPut :${BG} Vfat configuration file not found but Still you can proceed for migration ${m100}${NC}"
+    fi
+}
+
+function check_vfat(){
+    lsmod | grep vfat
+    errorCode2=${?}
+
+    if [[ ${errorCode2} != 0 ]]
+    then 
+    echo -e "Vfat not Found,checking configuration"
+    check_vfat_cfg
+    else 
+    echo -e "\n${BY}OutPut :${BG} Vfat already enabled ${m100}${NC}"
+    fi
+}
+
+go(){
+    add_password
+    check_vfat
+}
+
+case ${os_flavour_type} in 
+"Ubuntu") go ;; 
+"CentOS") go ;; 
+"SUSE") echo -e "${BY}OutPut :${BR} SUSE linux not supported${NC}" ;; 
+"Red") go ;; 
+"Oracle") echo -e "${BY}OutPut :${BR} Oracle linux not supported currently${NC}" ;; 
+"Amazon") go ;; 
+*) echo -e "${BY}OutPut :${BR} OS Type not detected ${NC}";; 
 esac
+
+#echo -e "${BY}OutPut :${BR} Amazon linux not supported currently${NC}"
+#echo -e "${BY}OutPut :${BR} Ubuntu linux not supported currently${NC}"
--- shell_script/vfat_password_lock/main.bash
+++ shell_script/vfat_password_lock/main.bash
@@ -1,11 +1,8 @@
 #!/bin/bash
-source ./var.bash
 
 function initializer () {
-
     > final.txt
     > failed_server.txt
-    > is_installed_server.text
     > count.txt
     mkdir -p log
     count=0
@@ -14,67 +11,104 @@ function initializer () {
 }
 initializer
 
-function executer (){
-    timeout 60s ssh ${orgument} -i ${key} ec2-user@${1} 'sudo -n bash -s' < check_vfat.bash;
+ssh_user='ec2-user' # change user here
+key=./key.pem       # Change pem key here
+port=22             # Optional
+orgument='-q -o BatchMode=yes -o StrictHostKeyChecking=no'
+ldd=`printf "%105s"`
+
+BR='\033[1;31m'     # ${BR}
+BG='\033[1;32m'     # ${BG}
+NC='\033[0m'        # ${NC}
+BY='\033[1;33m'     # ${BY}
+mark100='\U01F4AF'
+cross='\u274c'
+
+function progressbar {
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+    _fill=$(printf "\033[1;32m%${_done}s")
+    _empty=$(printf "\033[0m%${_left}s")
+    printf "\rProgress : \033[1;33m[${_fill// /#}${_empty// / }\033[1;33m]\033[1;32m ${_progress}%%${NC}"
 }
 
+function executer (){
+    timeout 35s ssh ${orgument} -i ${key} ${ssh_user}@${1} 'sudo -n bash -s' < check_vfat.bash;
+}
 
 function show_report (){
-    echo "================================== Executed show report Function ======================================"
+    echo -e "\n================================== Showing Logs ======================================\n"
     count=`awk 'END { print NR }' count.txt`  
     while [ ${count} != ${num_ip} ];
     do
         count=`awk 'END { print NR }' count.txt`  
         progressbar ${count} ${num_ip}
     done
-    echo -e "\n Showing Report\n"
-    echo -e "\n${BG} Suscessfully Fetched Unavailable Package Details for Below server.You can also check file final.txt ${mark100}${NC}"
-    awk '{print NR " -",$0}' final.txt
-    echo -e "\n${BR} Failed to pull Package Details for below servers because of network or login issue,please check log dir for more info ${cross}${NC}"
-    awk '{print NR " -",$0}' failed_server.txt
-    echo -e "\n${BG} Package present for Below server.You can also check file is_installed_server.text ${mark100}${NC}"
-    awk '{print NR " -",$0}' is_installed_server.text
+
+    final=`cat final.txt`
+    if [[ -z ${final} ]]
+    then
+        echo -e ""
+    else
+        echo -e "\n Showing Report\n"
+        echo -e "\n${BG}Successfully Fetched Output for below server ${mark100}${NC}"
+        printf "${ldd// /-}"
+        awk '{print NR " -",$0}' final.txt
+        printf "${ldd// /-}"
+    fi
+
+    failed_server=`cat failed_server.txt`
+    if [[ -z ${failed_server} ]]
+    then
+        echo -e ""
+    else
+        echo -e "\n${BR}Unable to pull Output for below server ${cross}${NC}"
+        printf "${ldd// /-}"
+        awk '{print NR " -",$0}' failed_server.txt
+        printf "${ldd// /-}"
+    fi
+    echo -e "\n${BY}You can also check "log" dir for individual server log ${NC}"
     tput sgr0
 }
 
-echo -e "#############################\n"
+echo -e "Executed Main Function"
 
-function vfat_check() { # Where are you going ,look at here
+function vfat_check() { 
 
-    echo -e "${BY} Checking package for ${1}${NC}"
-    log_path=${PWD}/log/pkg_result_${1}
-    executer ${1} "echo "${2}" | base64 -d > check.sh && sudo bash check.sh" > ${log_path}
+    echo -e "${BY} Checking Vfat and Password lock for ${1}${NC}"
+    log_path=${PWD}/log/${1}_vfat_output.txt
+    > ${log_path}
+    executer ${1} >> ${log_path}
     errorCode1=${?}
 
     if [[ ${errorCode1} = 0 ]]
     then
-        is_installed=`awk '/is_installed/ {print $1}' ${log_path}`
-        not_installed=`awk '/not_installed/ {print $1}' ${log_path}`
-        echo ${1} ${not_installed} >> final.txt
-        echo ${1} ${is_installed} >> is_installed_server.text
-        echo "executed" >> count.txt
+        is_output=`grep -i "output"  ${log_path}`
+        echo ${1} ${is_output} >> final.txt
     else
-        echo "executed" >> count.txt
-        echo -e "${BR} Connection or Something error for ${1} ${cross}${BY} moving to next server ${NC}\n"
+        echo -e "${BR} Connection or Some error for ${1} ${cross}${BY} moving to next server ${NC}\n"
         echo -e ${1} >> failed_server.txt
     fi
+    echo "executed" >> count.txt
 }
 
-function server_ip(){
+function execute_main_ip(){
 
     initializer
     list_server=`cat server_list.txt`
     if [[ -z ${list_server} ]]
-        then
-            echo -e "${BR} Server list is empty ${cross}\n${BY} please fill server_list${NC}"
-            exit 1
-        else
-            for ip in ${list_server}; do
-            vfat_check ${ip} & # line execution and thread
+    then
+        echo -e "${BR} Server list is empty ${cross}\n${BY} please fill server_list${NC}"
+        exit 1
+    else
+        for ip in ${list_server}; do
+            vfat_check ${ip} & # Single execution and thread
             let num_ip++
-            done
-            show_report
-        fi
+        done
+    fi
+    sleep 0.01
+    show_report
 }
 
-server_ip
+execute_main_ip
