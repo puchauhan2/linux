6e4c3581b97e23ddc603554a89ab5d0bec63d5e6
Puneet Chauhan <puchauhan4@gmail.com>
2024-04-28 18:22:01 +0530

    Create ls_command_reference.bash
    added ls reference

---
+++ shell_script/ls_command_reference.bash
@@ -0,0 +1,4 @@
+ls {*.doc,*.pdf,*.txt} # will search file of mentioned format
+ls m[0-9]t # will search file like m1t,m9t 
+ls *[0-9]* # will search file name contains numbers
+ls test_[!9] # match file does not have 9 
